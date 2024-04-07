6922d15148ef5da2c4efbc0d7f15ad76bd92bd59
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-07 23:43:05 +0300

    [FEATER] add user request router

--- app/routes/user.controller.js
+++ app/routes/user.controller.js
@@ -3,10 +3,13 @@ import UserController from '../controllers/user.controller.js';
 
 const router = new Express();
 
-router.post('/user', UserController.createUser)
-router.delete('/user', UserController.createUser)
-router.post('/user', UserController.createUser)
-router.post('/user', UserController.createUser)
-router.post('/user', UserController.createUser)
+router.post('/user', (req, res) => {
+    const { username, firstName, lastName, password } = req.body;
+    console.log(username, firstName, lastName, password)
+    res.json("Ok");
+})
+// router.get('/user/:username', UserController.getUser)
+// router.put('/user/:username/:option', UserController.updateUser)
+// router.delete('/user/:username', UserController.deleteUser)
 
 export default router;
