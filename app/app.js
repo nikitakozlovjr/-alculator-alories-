92d4ad4d3f368e6f2bfa7d5a551bc8db1d90f464
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-07 23:39:14 +0300

    [REFACTORING]  change structure main app

--- app/app.js
+++ app/app.js
@@ -1,8 +1,17 @@
 import Express from 'express';
 import bodyParser from 'body-parser';
+import userRedirection from './routes/routes.redirection.js';
+import userRoutes from './routes/user.controller.js';
+import methodOverride from 'method-override';
 
-const app = new Express();
-app.set('view engine', 'pug');
+const app = Express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.set('view engine', 'pug');
+
+app.use("/api", userRoutes);
+app.use(userRedirection)
 
 export default app;
