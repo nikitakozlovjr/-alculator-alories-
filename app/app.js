99c45471b2a5de04667f96bf319902a8653fade7
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-11 00:30:25 +0300

    [FEATURE] add json parse

--- app/app.js
+++ app/app.js
@@ -1,13 +1,14 @@
 import Express from 'express';
-import bodyParser from 'body-parser';
 import userRedirection from './routes/routes.redirection.js';
-import userRoutes from './routes/user.controller.js';
+import userRoutes from './routes/user.routes.js';
 import methodOverride from 'method-override';
+import bodyParser from 'body-parser';
+import express from 'express';
 
 const app = Express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(express.json())
 
 app.set('view engine', 'pug');
 
