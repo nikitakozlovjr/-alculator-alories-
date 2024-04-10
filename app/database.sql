6dbb685c0fcb6d785e2d22f72dbe3c58a7d95fad
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-11 00:31:07 +0300

    [REFACTORING] change NOT NULL UNIQUE -> PRIMARY KEY

--- app/database.sql
+++ app/database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    username VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
