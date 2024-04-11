e9db2a08b98bcbc38e2cd75b24d3a4f6c45216c6
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-11 23:09:09 +0300

    [FEATURE] add routes user data change and get

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,52 +1,20 @@
 import Express from 'express';
-import db from '../db.js';
-import Encrypt from '../../Encrypt.js'
+import UserController from '../controllers/user.controller.js';
 
 const router = new Express();
 
-router.post('/user', async (req, res) => {
-    const { username, firstName, lastName, password } = req.body;
-    const hashPassword = new Encrypt().hash(password);
+router.post('/user', await new UserController().createUser);
 
-    const newPerson = await db.query(`INSERT INTO users (username, first_name, last_name, password) VALUES ($1, $2, $3, $4) RETURNING *`, [username, firstName, lastName, hashPassword]);
-    res.json(newPerson.rows[0]);
-});
+router.get('/user/:username', await new UserController().getUser);
 
-router.get('/user/:username', async(req, res) => {
-    const username = req.params.username;
+router.put('/user/:username/firstname', await new UserController().updateFirstNameUser);
 
-    const personData = await db.query('SELECT * FROM users WHERE username = $1', [username]);
-    res.json(personData.rows[0])
-});
+router.put('/user/:username/lastname', await new UserController().updateLastNameUser);
 
-router.put('/user/:username/first_name/:newData', async (req, res) => {
-    const username = req.params.username;
-    const newData = req.params.newData;
+router.put('/user/:username/username', await new UserController().updateUsernameUser);
 
-    const updateDataUser = await db.query('UPDATE users SET first_name = $1 WHERE username = $2 RETURNING *', [newData, username]);
-    res.json(updateDataUser.rows[0]);
-});
-router.put('/user/:username/last_name/:newData', async (req, res) => {
-    const username = req.params.username;
-    const newData = req.params.newData;
+router.put('/user/:username/password', await new UserController().updatePasswordUser);
 
-    const updateDataUser = await db.query('UPDATE users SET last_name = $1 WHERE username = $2 RETURNING *', [newData, username]);
-    res.json(updateDataUser.rows[0]);
-});
-router.put('/user/:username/username/:newData', async (req, res) => {
-    const username = req.params.username;
-    const newData = req.params.newData;
-
-    const updateDataUser = await db.query('UPDATE users SET username = $1 WHERE username = $2 RETURNING *', [newData, username]);
-    res.json(updateDataUser.rows[0]);
-});
-router.put('/user/:username/password/:newData', async (req, res) => {
-    const username = req.params.username;
-    const newData = new Encrypt().hash(req.params.newData);
-
-    const updateDataUser = await db.query('UPDATE users SET password = $1 WHERE username = $2 RETURNING *', [newData, username]);
-    res.json(updateDataUser.rows[0]);
-});
-// router.delete('/user/:username', UserController.deleteUser)
+router.delete('/user/:username', await new UserController().deleteUser);
 
 export default router;
