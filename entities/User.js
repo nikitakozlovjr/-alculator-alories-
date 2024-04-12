3e42e162eaf6f051be438584228822462bbcee25
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-12 16:30:38 +0300

    [REFACTORING] add new entity properties User

--- entities/User.js
+++ entities/User.js
@@ -1,8 +1,26 @@
 class User {
-    constructor(nickName, password) {
-        this.nickName = nickName;
+    constructor(username, firstName, lastName, password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.password = password;
         this.quest = false
+    }
+
+    getUsername() {
+        return this.username;
+    }
+
+    getFirstName() {
+        return this.firstName;
+    }
+
+    getLastName() {
+        return this.lastName;
+    }
+
+    getPassword() {
+        return this.password;
     }
 
     isGuest() {
