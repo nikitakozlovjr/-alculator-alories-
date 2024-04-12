0e83acfcfdb67a5c9d9e4fee3e30f1a8b48ba137
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-12 16:29:10 +0300

    [CHORE] add link start server

--- bin/server.js
+++ bin/server.js
@@ -2,5 +2,5 @@ import app from "../app/app.js"
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server has been started on ${port} port!`)
+    console.log(`Server has been started on http://localhost:${port}`)
 })
