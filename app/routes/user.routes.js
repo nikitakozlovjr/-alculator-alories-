9cac4a3718a0834919853a083cec4ade7baeccd4
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-19 20:27:37 +0300

    [STYLE] change margin

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,18 +3,18 @@ import UserController from '../controllers/user.controller.js';
 
 const router = new Express();
 
-router.post('/user', await new UserController().createUser);
+router.post('/users', await new UserController().createUser);
 
-router.get('/user/:username', await new UserController().getUser);
+router.get('/users/:username', await new UserController().getUsers);
 
-router.put('/user/:username/firstname', await new UserController().updateFirstNameUser);
+router.put('/users/:username/firstname', await new UserController().updateFirstNameUser);
 
-router.put('/user/:username/lastname', await new UserController().updateLastNameUser);
+router.put('/users/:username/lastname', await new UserController().updateLastNameUser);
 
-router.put('/user/:username/username', await new UserController().updateUsernameUser);
+router.put('/users/:username/username', await new UserController().updateUsernameUser);
 
-router.put('/user/:username/password', await new UserController().updatePasswordUser);
+router.put('/users/:username/password', await new UserController().updatePasswordUser);
 
-router.delete('/user/:username', await new UserController().deleteUser);
+router.delete('/users/:username', await new UserController().deleteUser);
 
 export default router;
