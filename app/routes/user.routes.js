ac77e8b4a2b6e906670541c944dc1eb62668d3c0
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-26 18:07:47 +0300

    [STYLE] delete users point for url link

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,18 +3,18 @@ import UserController from '../controllers/user.controller.js';
 
 const router = new Express();
 
-router.post('/users', await new UserController().createUser);
+router.post('/', await new UserController().createUser);
 
-router.get('/users/:username', await new UserController().getUsers);
+router.get('/:username', await new UserController().getUsers);
 
-router.put('/users/:username/firstname', await new UserController().updateFirstNameUser);
+router.put('/:username/firstname', await new UserController().updateFirstNameUser);
 
-router.put('/users/:username/lastname', await new UserController().updateLastNameUser);
+router.put('/:username/lastname', await new UserController().updateLastNameUser);
 
-router.put('/users/:username/username', await new UserController().updateUsernameUser);
+router.put('/:username/username', await new UserController().updateUsernameUser);
 
-router.put('/users/:username/password', await new UserController().updatePasswordUser);
+router.put('/:username/password', await new UserController().updatePasswordUser);
 
-router.delete('/users/:username', await new UserController().deleteUser);
+router.delete('/:username', await new UserController().deleteUser);
 
 export default router;
