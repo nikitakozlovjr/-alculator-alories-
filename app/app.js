e62110b0282bccd7b0adbd9336555d33c64310c7
nikitakozlovjr <nikita3726898@gmail.com>
2024-05-03 18:35:25 +0300

    [FEATURE] add posts routes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import Express from 'express';
 import userRedirection from './routes/routes.redirection.js';
 import userRoutes from './routes/user.routes.js';
+import postRouter from './routes/post.routes.js';
 import sessionRoutes from './routes/session.routes.js';
 import methodOverride from 'method-override';
 import bodyParser from 'body-parser';
@@ -28,6 +29,7 @@ app.set('view engine', 'pug');
 
 app.use('/session', sessionRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRouter);
 app.use(userRedirection);
 
 export default app;
