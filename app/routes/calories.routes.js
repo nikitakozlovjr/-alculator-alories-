f596fa7f9964df4af795a16657b2538000449263
nikitakozlovjr <nikita3726898@gmail.com>
2024-05-07 16:53:14 +0300

    [FIXTURE] add ruter save for get data calculations

--- app/routes/calories.routes.js
+++ app/routes/calories.routes.js
@@ -1,11 +1,26 @@
 import Express from 'express';
+import activeCoefficients from '../utils/calories/activeCoefficients.js';
+import genderCoefficients from '../utils/calories/genderCoefficients.js';
+import getCalculation from '../utils/calories/getCalculation.js';
 
 const router = new Express();
 
 router.post('/calculation', (req, res) => {
     const { gender, age, height, weight, active } = req.body;
+    const { currentUser } = res.locals;
+    const genderCoefficient = genderCoefficients[gender];
+    const activeCoefficient = activeCoefficients[active];
+    const calculations = getCalculation(activeCoefficient, weight, height, age, genderCoefficient);
+    const data = {title: "Ваша норма калориев", currentUser, calculations};
+    res.render('results/show', data)
+});
 
-    res.redirect('/calories')
+router.post('/save', (req, res) => {
+    const { normal, low, more } = req.query;
+    console.log(parseInt(normal))
+    console.log(parseInt(low))
+    console.log(parseInt(more))
+    res.redirect('/')
 })
 
 export default router;
