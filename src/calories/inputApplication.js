fe3df38783f093bd9a19e80f562d6f8b9b1d245e
nikitakozlovjr <nikita3726898@gmail.com>
2024-05-07 13:15:15 +0300

    [FEATURE] add reset form and change validaate form

--- src/calories/inputApplication.js
+++ src/calories/inputApplication.js
@@ -69,6 +69,11 @@ const app = () => {
             render(state);
         })
     });
+
+    elements.reset.addEventListener('click', () => {
+        state.validate = false;
+        render(state);
+    });
 };
 
 app()
