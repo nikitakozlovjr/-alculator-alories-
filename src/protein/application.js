194cc339e0aa136713a14ffa00c44fe7cdd796f5
nikitakozlovjr <nikita3726898@gmail.com>
2024-05-09 11:21:46 +0300

    [FEATURE] add application funcion for protein page

---
+++ src/protein/application.js
@@ -0,0 +1,75 @@
+const validateFormCheck = (state) => {
+    const inputsValues = Object.values(state.values);
+    const resultCheck = inputsValues.every((value) => {
+        if (!value) {
+            return false;
+        };
+
+        if (value.trim().length === 0) {
+            return false;
+        };
+        
+        return true;
+    });
+
+    return resultCheck;
+};
+
+const checkValuesInput = (state) => {
+    const inputsValues = Object.values(state.values);
+    const resultCheck = inputsValues.some((value) => {
+        if(!value) {
+            return false;
+        };
+        return true;
+    });
+
+    return resultCheck;
+}
+
+const elements = {
+    inputs: document.querySelectorAll('[type="text"]'),
+    weight: document.querySelector('[name="weight"]'),
+    submit: document.querySelector('[name="submit"]'),
+    reset: document.querySelector('[name="reset"]')
+};
+
+const render = (state) => {
+    if(state.validate) {
+        elements.submit.disabled = false;
+    };
+    if(!state.validate) {
+        elements.submit.disabled = true;
+    };
+    if(checkValuesInput(state)) {
+        elements.reset.disabled = false;
+    };
+    if(!checkValuesInput(state)) {
+        elements.reset.disabled = true;
+    } 
+};
+
+const app = () => {
+    const state = {
+        values: {
+            weight: null
+        },
+        validate: false
+    };
+
+    elements.inputs.forEach((input) => {
+        input.addEventListener('input', (event) => {
+            const value = event.target.value;
+            state.values[event.target.name] = value;
+            state.validate = validateFormCheck(state);
+            render(state);
+        })
+    });
+
+    elements.reset.addEventListener('click', () => {
+        state.validate = false;
+        render(state);
+    });
+};
+
+app()
